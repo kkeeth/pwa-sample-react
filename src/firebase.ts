1f66f2644f8f468e13cbaea294b87a4fcfaba9fa
Keeth Kuwahara <kiyohito.kuwahara@kaminashi.com>
2024-04-13 23:53:39 +0900

    wip: FCM

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,38 @@
 import { initializeApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging';
+import { getMessaging, getToken } from 'firebase/messaging';
 
 import type { FirebaseApp } from 'firebase/app';
 import type { Messaging } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgHLq3Fbkq9LXl73lgMeqBhNv93x2ZlYc",
-  authDomain: "keeth-playground.firebaseapp.com",
-  projectId: "keeth-playground",
-  storageBucket: "keeth-playground.appspot.com",
-  messagingSenderId: "69915345896",
-  appId: "1:69915345896:web:5f47c873ab725f9b9a9a5d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
-
 const messaging: Messaging = getMessaging(app);
+
+
+console.log(navigator)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((sw) => {
+      console.log(sw)
+      getToken(messaging, {
+        vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
+        serviceWorkerRegistration: sw,
+      }).then((token) => {
+        console.log(token)
+        });
+    })
+    .catch((err) => {
+      console.error('Service Worker registration failed', err);
+    });
+}
+
 
 export { messaging };
--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Workbox } from 'workbox-window';
 import App from './App.tsx'
 import './index.css'
-import { messaging } from './firebase'; // 追加
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-
-if ('serviceWorker' in navigator) {
-  const wb = new Workbox('/sw.js');
-
-  wb.addEventListener('activated', async (event) => {
-    if (!event.isUpdate) {
-      console.log('Service worker activated for the first time!');
-
-      // Service Worker がアクティブになったら通知許可を求める
-      const permission = await Notification.requestPermission();
-      if (permission === 'granted') {
-        messaging.getToken().then((token) => {
-          console.log('Token:', token);
-        });
-      }
-    }
-  });
-
-  wb.register();
-}
